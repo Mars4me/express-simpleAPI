8178844dcc2b303fd666ba31b98ce486e3a6e146
VP <lmarsl1994@gmail.com>
2022-02-15 19:49:33 +0200

    add: add route, controller, service

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import router from "./router.js";
+import fileUpload from 'express-fileupload'
+
+const PORT = 5000
+const DB_URL = 'mongodb+srv://qwe:123@cluster0.i8ry5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const app = express();
+
+app.use(express.json())
+app.use('/api', router)
+
+const start = async () => {
+    try {
+        await mongoose.connect(DB_URL)
+        app.listen(PORT, () => {
+            console.log(`Server started on port ${PORT}`)
+        })
+    } catch (e) {
+        console.log(e)
+    }
+
+}
+
+start()
